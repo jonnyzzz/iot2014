71bff8dad0e442e2947e88f8300b953eaf148545
Eugene Petrenko <eugene.petrenko@gmail.com>
2014-10-18 16:17:06 +0200

    code style

--- server/db.js
+++ server/db.js
@@ -1,17 +1,18 @@
-var DB = {};
+var DB = [];
 var version = 0;
 
-exports.save = function(board, sensor, type, value) {
-  console.log("Reported data: " + board + ":" + sensor + " [" + type + "] --> " + value);
-  version++;
+exports.save = function (board, sensor, type, value) {
+    console.log("Reported data: " + board + ":" + sensor + " [" + type + "] --> " + value);
+    version++;
+
+
 };
 
-exports.version = function() {
+exports.version = function () {
     return "" + version;
 };
 
-exports.dump = function() {
+exports.dump = function () {
     return JSON.stringify(DB);
 };
-
 
